2ff996a2e0bbc183b9522e5a5fe97265e8442a97
rizaadi <rizaadi890@gmail.com>
2023-09-09 10:19:29 +0700

    codelab 2

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -5,11 +5,13 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Image.asset('images/farm-house.jpg'),
             Container(
               padding: const EdgeInsets.only(top: 16.0),
               child: const Text(
@@ -18,6 +20,7 @@ class DetailScreen extends StatelessWidget {
                 style: TextStyle(
                   fontSize: 30.0,
                   fontWeight: FontWeight.bold,
+                  fontFamily: 'Staatliches',
                 ),
               ),
             ),
@@ -28,23 +31,32 @@ class DetailScreen extends StatelessWidget {
                 children: [
                   Column(
                     children: [
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday'),
+                      const Icon(Icons.calendar_today),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        'Open Everyday',
+                        style: informationTextStyle,
+                      ),
                     ],
                   ),
                   Column(
                     children: [
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8.0),
-                      Text('09:00 - 20:00'),
+                      const Icon(Icons.access_time),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        '09:00 - 20:00',
+                        style: informationTextStyle,
+                      ),
                     ],
                   ),
                   Column(
                     children: [
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8.0),
-                      Text('RP 25.000'),
+                      const Icon(Icons.monetization_on),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        'RP 25.000',
+                        style: informationTextStyle,
+                      ),
                     ],
                   ),
                 ],
@@ -58,6 +70,33 @@ class DetailScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 16.0),
               ),
             ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg')),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network('https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg')),
+                  ),
+                ],
+              ),
+            ),
+            Image.network('https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
           ],
         ),
       ),
